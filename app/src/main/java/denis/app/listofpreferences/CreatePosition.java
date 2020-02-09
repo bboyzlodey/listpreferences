eec2a716f4287662d6d5f6a02d87bf0a889294c6
asybil <skarlat-denis@mail.ru>
2020-02-09 23:27:18 +0300

    create new class

---
+++ app/src/main/java/denis/app/listofpreferences/CreatePosition.java
@@ -0,0 +1,50 @@
+package denis.app.listofpreferences;
+
+import java.util.Arrays;
+
+public class CreatePosition{
+    StringBuilder [] create;
+
+    /*
+    *
+    * one == 1 the be article
+     */
+    CreatePosition(int one){
+        if (one == 1)
+        {
+            create = new StringBuilder[15];
+        }
+    }
+    public StringBuilder concat(StringBuilder [] conc) {
+        int i;
+        StringBuilder tmp = new StringBuilder();
+
+        i = conc.length;
+
+        while (i != conc.length) {
+            tmp.append(conc[i]);
+            i++;
+        }
+        return tmp;
+    }
+    public String concatString(StringBuilder [] conc){
+        return (Arrays.toString(conc));
+    }
+    public String concatString(){
+        return (Arrays.toString(this.create));
+    }
+    public String fill(StringBuilder [] src){
+        int i = 0;
+        int j = 0;
+
+        while (i != this.create.length) {
+            if ((this.create[i].toString() == null) && j != src.length)
+            {
+                this.create[i] = src[j];
+                j++;
+            }
+            i++;
+        }
+        return (this.concatString());
+    }
+}
